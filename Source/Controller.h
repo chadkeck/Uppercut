a88a5870202677bd7508ae226b12a39ba1d2a239
Chad Bibler <chad@chadbibler.com>
2024-12-21 06:37:18 -0600

    First #pragma mark-ing

--- Source/Controller.h
+++ Source/Controller.h
@@ -13,6 +13,16 @@
 	TcpClient *_client;
 }
 
+#pragma mark - UI Actions
 - (IBAction)onClick:(id)sender;
+
+#pragma mark - Connection Status
 - (void)setConnectionLightState:(NetworkStatusState)state;
+
+#pragma mark - TcpClientDelegate
+- (void)tcpClientDidConnect:(id)client;
+- (void)tcpClient:(id)client didReceiveData:(NSData *)data;
+- (void)tcpClient:(id)client didFailWithError:(NSError *)error;
+- (void)tcpClientDidDisconnect:(id)client;
+
 @end
