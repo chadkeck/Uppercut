057e3230627cbd2fc5f700245f1a7d4f21131945
Chad Bibler <chad@chadbibler.com>
2024-12-22 17:09:18 -0600

    Better logging and handle TCP server disconnect

--- Source/Controller.m
+++ Source/Controller.m
@@ -18,11 +18,18 @@
 	[super dealloc];
 }
 
-- (IBAction)onClickConnect:(id)sender {	
+- (IBAction)onClickConnect:(id)sender {
+	NSString *host = [hostField stringValue];
+	int port = [portField intValue];
+	
+	// TODO: some validation on host and port
+	
 	[_client setHost:[hostField stringValue]];
 	[_client setPort:[portField intValue]];
-
+	
 	if (![_client isConnected]) {
+		[[Logger sharedInstance] log:[NSString stringWithFormat:@"Connecting to %@:%d", host, port]];
+
 		[_client connect];
 		[networkStatusController setConnectionState:NetworkStatusStateWaiting];
 	} else {
@@ -32,11 +39,16 @@
 
 - (IBAction)onClickSendData:(id)sender {
 	NSLog(@"send data clicked");
+	
+	if (![_client isConnected]) {
+		return;
+	}
 		
 	// send data
 	NSString *message = [[sendDataTextField stringValue] stringByAppendingString:@"\r\n"];
 	NSData *data = [message dataUsingEncoding:NSUTF8StringEncoding];
 	[_client sendData:data];
+	
 	[[Logger sharedInstance] log:[NSString stringWithFormat:@"Sent message: %@", message]];
 	
 	// clear the input field
@@ -45,6 +57,8 @@
 
 // TCPClientDelegate methods
 - (void)tcpClientDidConnect:(id)client {
+	[[Logger sharedInstance] log:@"Connected"];
+	
 	[connectButton setTitle:@"Disconnect"];
 	[hostField setEnabled:FALSE];
 	[portField setEnabled:FALSE];
@@ -55,15 +69,23 @@
 - (void)tcpClient:(id)client didReceiveData:(NSData *)data {
 	NSString *message = [[NSString alloc] initWithData:data 
 											  encoding:NSUTF8StringEncoding];
-	NSLog(@"Received message: %@", message);
+
+	// check for disconnect
+	if ([data length] == 0) {
+		[self tcpClientDidDisconnect:client];
+	} else {
+		[[Logger sharedInstance] log:[NSString stringWithFormat:@"Received message: %@", message]];
 	
-	[dataTextView setString:message];
+		[dataTextView setString:message];
+	}
 	
 	[message release];
 }
 
 - (void)tcpClient:(id)client didFailWithError:(NSError *)error {
 	NSLog(@"Connection failed with error: %@", error);
+	[[Logger sharedInstance] log:[NSString stringWithFormat:@"Connection failed with error: %@", error]];
+
 	[connectButton setTitle:@"Connect"];
 	[hostField setEnabled:TRUE];
 	[portField setEnabled:TRUE];
@@ -71,6 +93,8 @@
 }
 
 - (void)tcpClientDidDisconnect:(id)client {
+	[[Logger sharedInstance] log:@"Disconnected"];
+	
 	[connectButton setTitle:@"Connect"];
 	[hostField setEnabled:TRUE];
 	[portField setEnabled:TRUE];
