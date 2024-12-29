3c55ac551b5efb2737282bc225c9cce3615c9993
Chad Bibler <chad@chadbibler.com>
2024-12-29 07:11:07 -0600

    Version info for About dialog

--- main.m
+++ main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  Uppercut
-//
-//  Created by Chad Bibler on 12/20/24.
-//  Copyright __MyCompanyName__ 2024. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, char *argv[])
